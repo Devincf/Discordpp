c5507ae06e37e7ddf463c600a17e72f222a56bff
Devincf <devinc.firat@gmail.com>
2019-02-16 13:34:30 +0100

    uhh

---
+++ CMakeFiles/Discordpp.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Discordpp.dir/src/discordpp/Objects/Snowflake.cpp.o"
+  "CMakeFiles/Discordpp.dir/src/discordpp/Objects/User.cpp.o"
+  "CMakeFiles/Discordpp.dir/src/discordpp/Objects/Emoji.cpp.o"
+  "CMakeFiles/Discordpp.dir/src/discordpp/Objects/Role.cpp.o"
+  "CMakeFiles/Discordpp.dir/src/discordpp/Objects/Channel.cpp.o"
+  "CMakeFiles/Discordpp.dir/src/discordpp/Objects/Guild.cpp.o"
+  "CMakeFiles/Discordpp.dir/src/discordpp/Gateway.cpp.o"
+  "CMakeFiles/Discordpp.dir/src/discordpp/Bot.cpp.o"
+  "CMakeFiles/Discordpp.dir/src/example/main.cpp.o"
+  "Discordpp.pdb"
+  "Discordpp"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Discordpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
