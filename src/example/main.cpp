a0c337f9b2c022177b7778aa8b8d3b0484b7607c
Devincf <devinc.firat@gmail.com>
2019-02-20 02:16:38 +0100

    added example for timestamps

--- src/example/main.cpp
+++ src/example/main.cpp
@@ -8,11 +8,20 @@
  * @copyright Copyright (c) 2019
  * 
  */
-#include "../discordpp/Bot.hpp"
+#include "Bot.hpp"
+
+#include "Util/Timestamp.hpp"
 
 
 int main()
 {
+
+    /*discordpp::util::Timestamp t1 = discordpp::util::fromUnixTimestamp("1550622594");
+    discordpp::util::Timestamp t2 = discordpp::util::fromISO8601Timestamp("2019-02-20T00:29:56.162000+00:00");
+    std::cout << t1.getUnixTime() << std::endl;
+    std::cout << t2.getUnixTime() << std::endl;*/
+    
+
     const std::string token = "NTM5ODIwNDYyMjg1Mzg5ODI0.DzH9tw.AYjoVQwUZJywz28L_nZP716FeEI";
     discordpp::Bot b{token};
 
