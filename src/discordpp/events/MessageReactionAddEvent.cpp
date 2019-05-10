51234e8aef384e45ae316dd2b5ddda36aa3390ef
Devincf <devinc.firat@gmail.com>
2019-05-11 00:16:35 +0200

    Added automatic reaction distribution

--- src/discordpp/events/MessageReactionAddEvent.cpp
+++ src/discordpp/events/MessageReactionAddEvent.cpp
@@ -10,11 +10,22 @@
  */
 
 #include "MessageReactionAddEvent.hpp"
+
+#include <string>
+#include "util/Snowflake.hpp"
+#include "util/Singleton.hpp"
+#include "reactions/ReactionManager.hpp"
+
 namespace discordpp
 {
-    bool MessageReactionAddEvent::proc(const nlohmann::json& packet)
+bool MessageReactionAddEvent::proc(const nlohmann::json &packet)
+{
+    //DEBUG(packet.dump(2));
+    Snowflake id = packet["d"]["message_id"];
+    if(Singleton<ReactionManager>::get()->isReactableMessage(id))
     {
-        DEBUG(packet.dump(2));
-        return true;
+        Singleton<ReactionManager>::get()->getReaction(id)->proc(packet);
     }
+    return true;
 }
+} // namespace discordpp
